8fd2cd2770b48ebd40541805e75d1064ff3a606b
Lawrance <lawchen522@gmail.com>
2024-05-12 13:48:52 +0800

    feat: add signup page

---
+++ controllers/user-controller.js
@@ -0,0 +1,28 @@
+const { User } = require('../models')
+const bcrypt = require('bcryptjs')
+const userController = {
+  signUpPage: (req, res, next) => {
+    res.render('signup')
+  },
+  signUp: (req, res, next) => {
+    const { name, email, password, passwordCheck } = req.body
+    if (!name) throw new Error('請輸入名稱')
+    if (password !== passwordCheck) throw new Error('確認密碼錯誤')
+    User.findOne({ where: { email } })
+      .then(user => {
+        if (user) throw new Error('信箱已註冊')
+        return bcrypt.hash(password, 10)
+      })
+      .then(hash => {
+        return User.create({
+          name,
+          email,
+          password: hash
+        })
+      })
+      .then(() => res.render('signin'))
+      .catch(err => next(err))
+  }
+}
+
+module.exports = userController
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const router = express.Router()
 const admin = require('./modules/admin')
 const bookController = require('../controllers/book-controller')
+const userController = require('../controllers/user-controller')
 router.use('/admin', admin)
-
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 router.get('/books', bookController.getBooks)
 
 router.use('/', (req, res) => res.redirect('/books'))
